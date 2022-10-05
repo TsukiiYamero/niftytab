b7905351999d2a1907aca53dbb13bc7f3820c635
SaiKisho <diegosferas@gmail.com>
2022-10-05 16:34:05 -0500

    TabSelection styles

--- src/ui/organisms/TabsSelection/TabsSelection.tsx
+++ src/ui/organisms/TabsSelection/TabsSelection.tsx
@@ -1,16 +1,17 @@
 
 import { IconButtonTab } from "@/ui/atoms/Buttons"
 import { FoldersIcon, SessionIcon, TabsIcon } from "@/ui/atoms/icons"
+import { TabsSelectionWrapper } from './styledComponents/TabsSelectionWrapper.styled';
 
 type Props = {}
 
 const TabsSelection = (props: Props) => {
     return (
-        <div>
+        <TabsSelectionWrapper>
             <IconButtonTab text="Tabs" icon={<TabsIcon />} onClick={() => { }} />
             <IconButtonTab text="GroupTabs" icon={<FoldersIcon />} onClick={() => { }} />
             <IconButtonTab text="Sessions" icon={<SessionIcon />} onClick={() => { }} />
-        </div>
+        </TabsSelectionWrapper>
     )
 }
 
