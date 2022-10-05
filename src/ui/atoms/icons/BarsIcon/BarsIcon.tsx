e41593eea7eb406ef47468836ac5ee633955b68c
SaiKisho <diegosferas@gmail.com>
2022-10-05 16:33:15 -0500

    Filter icons & barrels

---
+++ src/ui/atoms/icons/BarsIcon/BarsIcon.tsx
@@ -0,0 +1,4 @@
+import { IconBaseProps } from 'react-icons'
+import { AiOutlineBars } from 'react-icons/ai'
+
+export const BarsIcon = (props: IconBaseProps) => <AiOutlineBars {...props} />;
---
+++ src/ui/atoms/icons/FilterIcon/FilterIcon.tsx
@@ -0,0 +1,4 @@
+import { IconBaseProps } from 'react-icons'
+import { IoFilter } from 'react-icons/io5'
+
+export const FilterIcon = (props: IconBaseProps) => <IoFilter {...props} />;
