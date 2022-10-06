8930a69ed34cf0dba019f13c973c4403fd23f5a4
SaiKisho <diegosferas@gmail.com>
2022-10-06 17:56:20 -0500

    Test fetch data tabs

--- src/ui/organisms/TabsContainer/TabsContainer.tsx
+++ src/ui/organisms/TabsContainer/TabsContainer.tsx
@@ -1,34 +1,54 @@
+import { getDataTavs } from "@/mocks/getDataTabs";
 import { TabSmallView } from "@/ui/atoms/TabSmallView"
+import { useEffect, useState } from "react"
 
-type Props = {}
+type ChromeTabs = {
+    active: boolean;
+    url: string;
+    discarded: boolean;
+    favIconUrl: string;
+    groupId: number;
+    id: number;
+    title: string;
+    index: number;
+    pinned: true;
+    selected: boolean;
+    mutedInfo?: { muted: boolean };
+    height?: number;
+    audible?: boolean;
+    highlighted?: boolean;
+    autoDiscardable?: boolean;
+    status?: string;
+    incognito?: boolean;
+    width?: number;
+    windowId?: number;
+}
 
-export const TabsContainer = (props: Props) => {
+export const TabsContainer = () => {
+
+    const [tabs, setTabs] = useState<ChromeTabs[]>();
+
+    useEffect(() => {
+        const getTabs = async () => {
+            const resp = await fetch('https://run.mocky.io/v3/bc8a8ae6-323e-4fac-bfcb-37f9d27713e9');
+            const dataTabs = await resp.json();
+            setTabs(dataTabs);
+        }
+        getTabs();
+
+    }, [])
+
+
     return (
-        <div style={{ display: 'flex', gap: '9px', paddingBlock: '10px', flexWrap: 'wrap' }}>
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
-            <TabSmallView
-                title="Container presentational pattern | javascript Patterns"
-                urlText="https://javascriptpatterns.vercel.app"
-                imgSrc="https://javascriptpatterns.vercel.app/logo.svg" />
+        <div style={{ display: 'flex', gap: '9px', paddingBlock: '10px', flexWrap: 'wrap', overflow: 'hidden' }}>
+            {
+                tabs && tabs.map((tab) => (<TabSmallView
+                    key={tab.id}
+                    title={tab.title}
+                    urlText={tab.url}
+                    imgSrc={tab.favIconUrl} />
+                ))
+            }
         </div>
     )
 }
