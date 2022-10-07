bf3ab917fa83a064a20e3327fdd6e5f78a22ddcf
SaiKisho <diegosferas@gmail.com>
2022-10-06 19:08:16 -0500

    Tab Small view adjusted

--- src/ui/atoms/TabSmallView/TabSmallView.tsx
+++ src/ui/atoms/TabSmallView/TabSmallView.tsx
@@ -12,8 +12,10 @@ type Props = {
 export const TabSmallView = ({ title, imgSrc, urlText }: Props) => {
     return (
         <div onClick={(ev) => createRipple(ev)} className="tab-Small-View__container" title={`${title} - ${urlText}`}>
-            <img src={imgSrc} alt={title} />
+            <img src={imgSrc} alt={'img of website not found'} />
+
             <span className={"overflow-ellipsis"}>{title}</span>
+
             <IconButtonSimple className="close-btn-simple">
                 <CloseIcon size={"18px"} />
             </IconButtonSimple>
