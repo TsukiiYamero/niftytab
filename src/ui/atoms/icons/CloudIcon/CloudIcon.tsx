ba258afd2399a32da8a48b0978448a431adb15d4
SaiKisho <diegosferas@gmail.com>
2022-10-20 00:05:58 -0500

    Cloud & laptop Icons

---
+++ src/ui/atoms/icons/CloudIcon/CloudIcon.tsx
@@ -0,0 +1,4 @@
+import { IconBaseProps } from 'react-icons';
+import { AiOutlineCloud } from 'react-icons/ai';
+
+export const CloudIcon = (props: IconBaseProps) => <AiOutlineCloud {...props} />;
---
+++ src/ui/atoms/icons/LaptopIcon/LaptopIcon.tsx
@@ -0,0 +1,4 @@
+import { IconBaseProps } from 'react-icons';
+import { MdOutlineLaptopMac } from 'react-icons/md';
+
+export const LaptopIcon = (props: IconBaseProps) => <MdOutlineLaptopMac {...props} />;
