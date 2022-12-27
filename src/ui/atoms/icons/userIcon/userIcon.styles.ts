7857a4d0e185a0066721f7c3aafd6a8abae5bea5
SaiKisho <diegosferas@gmail.com>
2022-12-27 15:28:01 -0500

    Removed img unused

--- src/ui/atoms/icons/userIcon/userIcon.styles.ts
+++ src/ui/atoms/icons/userIcon/userIcon.styles.ts
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
-import ImgUser from '../../../../assets/img/userIcon/artw.jpg';
 
 export const UserIcon = styled.div`
     border-radius: 5rem;
     background-color: gray;
     width: 46px;
     height: 46px;
-    background-image: url(${ImgUser});
     background-repeat: no-repeat;
     background-position: center;
     background-size: contain;
