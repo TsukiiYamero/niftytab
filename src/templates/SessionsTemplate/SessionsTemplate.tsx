633f21d7989fb2d253255b62650cfec49db6d358
SaiKisho <diegosferas@gmail.com>
2023-01-10 19:57:34 -0500

    Refactor Navigation to saved in sessions

--- src/templates/SessionsTemplate/SessionsTemplate.tsx
+++ src/templates/SessionsTemplate/SessionsTemplate.tsx
@@ -1,6 +1,9 @@
+import { useNavigateToSaved } from '@/customHooks/routes/useNavigateToLocalAndSaved';
 import { Outlet } from 'react-router-dom';
 
 export const SessionsTemplate = () => {
+    useNavigateToSaved();
+
     return (
         <Outlet />
     );
