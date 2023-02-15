8b0757a26064bc3e3c5bbbe8972910756ffa30c6
SaiKisho <diegosferas@gmail.com>
2023-02-14 19:28:33 -0500

    Refactor Initial form of Settings feature

---
+++ src/templates/Settings/Settings.tsx
@@ -0,0 +1,10 @@
+import { Settings } from '@/ui/molecules/Profile';
+import { Box } from '@mui/material';
+
+export const SettingsTemplate = () => {
+    return (
+        <Box>
+            <Settings />
+        </Box>
+    );
+};
