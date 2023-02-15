3c346d151880af54ceff9469405a331edc7310c1
SaiKisho <diegosferas@gmail.com>
2023-02-14 21:44:49 -0500

    Style Settings Items

--- src/ui/molecules/Settings/Settings.tsx
+++ src/ui/molecules/Settings/Settings.tsx
@@ -1,12 +1,17 @@
 import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const Settings = () => {
-    const listSettings = ['Password'];
+    const { pathname } = useLocation();
+    const listSettings = [{
+        path: 'password',
+        text: 'Password'
+    }];
 
     return (
         <Box>
             <Box>
-                <Typography variant='h2' sx={{
+                <Typography variant='h5' sx={{
                     fontWeight: 600,
                     color: 'var(--main-text-color)'
                 }}>
@@ -14,18 +19,32 @@ export const Settings = () => {
                 </Typography>
 
                 <List>
-
                     {
-                        listSettings.map((setting) =>
-                            <ListItem key={setting}>
-                                <ListItemText primary={setting} />
-                            </ListItem>
+                        listSettings.map(({ path, text }) =>
+                            <SettingsItem key={text} text={text} path={path} pathName={pathname} />
                         )
                     }
-
                 </List>
 
             </Box>
         </Box>
+    );
+};
+
+export const SettingsItem = ({ text, path, pathName }: { text: string, path: string, pathName: string }) => {
+    return (
+        <ListItem sx={{
+            padding: '2px 6px',
+            backgroundColor: pathName === `/settings/${path}` ? '#ccc9c952' : undefined,
+            borderRadius: '.3rem',
+            a: {
+                color: 'var(--main-text-color)'
+            }
+        }
+        }>
+            <NavLink to={`/settings/${path}`}>
+                <ListItemText primary={text} />
+            </NavLink>
+        </ListItem >
     );
 };
