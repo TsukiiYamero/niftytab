5d5ae9cf778bffaca1d2fba932112187775e0e74
SaiKisho <diegosferas@gmail.com>
2023-02-14 20:03:43 -0500

    Style Settings Template

--- src/templates/Settings/Settings.tsx
+++ src/templates/Settings/Settings.tsx
@@ -1,12 +1,48 @@
 import { Settings } from '@/ui/molecules/Settings';
-import { Box } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { ArrowBackIosNewOutlined } from '@mui/icons-material';
 
 export const SettingsTemplate = () => {
+    const navigateTo = useNavigate();
+
+    const goToHome = () => {
+        navigateTo('/');
+    };
+
     return (
-        <Box>
-            <Settings />
-            <Outlet></Outlet>
+        <Box sx={{
+            padding: '1.34rem'
+        }}>
+            <Box onClick={goToHome} sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '.85rem',
+                cursor: 'pointer'
+            }}>
+                <ArrowBackIosNewOutlined />
+                <Typography variant='h5'>Back</Typography>
+            </Box>
+
+            <Box sx={{
+                borderWidth: '1px',
+                borderColor: '#ccc9c952',
+                borderStyle: 'solid',
+                marginBlock: '1rem',
+                borderRadius: '1rem'
+            }}>
+
+            </Box>
+
+            <Box sx={{
+                display: 'grid',
+                gridTemplateColumns: '250px 1fr',
+                gap: '1.25rem',
+                paddingTop: '1.75rem'
+            }}>
+                <Settings />
+                <Outlet></Outlet>
+            </Box>
         </Box>
     );
 };
