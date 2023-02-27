09a694b8b92a4c3db01e5971fa9f50d91665cf39
SaiKisho <diegosferas@gmail.com>
2023-02-27 14:53:36 -0500

    Feature UI Comp Line

---
+++ src/ui/atoms/CommonLine/CommonLine.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material';
+
+export const CommonLine = () => {
+    return (
+        <Box sx={{
+            borderWidth: '1px',
+            borderColor: '#ccc9c952',
+            borderStyle: 'solid',
+            marginTop: '1.5rem',
+            marginBottom: '1rem',
+            borderRadius: '1rem'
+        }}>
+        </Box>
+    );
+};
