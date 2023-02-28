ff6390d2a7aa0833218326d873de57ef1d563413
SaiKisho <diegosferas@gmail.com>
2023-02-27 20:11:53 -0500

    Refactor Css fixed height

--- src/pages/mainPage/MainPageWrapper.styled.ts
+++ src/pages/mainPage/MainPageWrapper.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MainPageWrapper = styled.div`
     display: grid;
-    grid-template-rows: auto auto 1fr;
+    grid-template-rows: auto auto 354px;
     padding: 16px;
     width: 712px;
     height: 568px;
