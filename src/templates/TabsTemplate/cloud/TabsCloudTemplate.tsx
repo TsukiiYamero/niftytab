3d19e5d369a5fe7cbdc486813decf0b1671e6a03
SaiKisho <diegosferas@gmail.com>
2023-02-27 20:13:56 -0500

    Refactor data not needed

--- src/templates/TabsTemplate/cloud/TabsCloudTemplate.tsx
+++ src/templates/TabsTemplate/cloud/TabsCloudTemplate.tsx
@@ -1,15 +1,12 @@
-import { NiftyTab } from '@/models';
 import { useGetTabsContext, useTabsDispatch } from '@/contexts/tabs/hooks';
 import { MemoizedTabsListingsCloud } from '@/ui/organisms/TabsListings';
 
 export const TabsCloudTemplate = () => {
-    const { cloud, loading, isFiltering, filtered } = useGetTabsContext();
+    const { cloud, loading } = useGetTabsContext();
     const dispatch = useTabsDispatch();
 
     return <MemoizedTabsListingsCloud
         cloud={cloud}
-        filtered={(filtered as NiftyTab[])}
-        isFiltering={isFiltering}
         loading={loading}
         dispatch={dispatch} />;
 };
