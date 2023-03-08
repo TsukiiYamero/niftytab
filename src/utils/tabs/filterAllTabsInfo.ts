536a585e9a70268896b22e78e0495cb6d1618736
SaiKisho <diegosferas@gmail.com>
2023-03-07 19:56:52 -0500

    Fixed Bug controlling when there is not items

--- src/utils/tabs/filterAllTabsInfo.ts
+++ src/utils/tabs/filterAllTabsInfo.ts
@@ -7,7 +7,7 @@ export const filterAllTabsInfo = (AllTabsInfo: AllTabsInfo[] = [], query: string
     AllTabsInfo.forEach(group => {
         const listItemsFound = filterTabsByTitleOrUrl(group.tabs, query);
 
-        if (listItemsFound.length < 0) return;
+        if (listItemsFound.length === 0) return;
 
         newList.push({
             ...group,
