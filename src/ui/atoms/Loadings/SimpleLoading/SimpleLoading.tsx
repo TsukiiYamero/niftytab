54024cf81e4bd49969aaa119b1d5a5ee4d2c6d9c
SaiKisho <diegosferas@gmail.com>
2023-03-22 16:19:06 -0500

    Loader Feature

--- src/ui/atoms/Loadings/SimpleLoading/SimpleLoading.tsx
+++ src/ui/atoms/Loadings/SimpleLoading/SimpleLoading.tsx
@@ -1,5 +1,17 @@
+import { useRive } from '@rive-app/react-canvas';
+
+// import RIVER_LOADER from '@/assets/rive/loaders/simple-loader.riv';
+
 export const SimpleLoading = () => {
+    const { RiveComponent } = useRive({
+        src: '/src/assets/rive/loaders/flame-loader.riv',
+        autoplay: true
+    });
+
     return (
-        <div>Loading...</div>
+        <>
+            {/* credits for @JcToon and DC at https://rive.app/@dc/ */}
+            <RiveComponent />
+        </>
     );
 };
