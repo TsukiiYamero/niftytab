8b5ca470d7d1dfaaa643ef71bd8fac7c28d9330b
SaiKisho <diegosferas@gmail.com>
2023-03-22 20:32:55 -0500

    Refactor Content not Found ~ rive created

--- src/ui/atoms/Loadings/SimpleLoading/SimpleLoading.tsx
+++ src/ui/atoms/Loadings/SimpleLoading/SimpleLoading.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { useRive } from '@rive-app/react-canvas';
 
 // import RIVER_LOADER from '@/assets/rive/loaders/simple-loader.riv';
@@ -9,9 +10,19 @@ export const SimpleLoading = () => {
     });
 
     return (
-        <>
-            {/* credits for @JcToon and DC at https://rive.app/@dc/ */}
-            <RiveComponent />
-        </>
+        <Box sx={{
+            width: '100%',
+            height: '100%',
+            display: 'grid',
+            placeContent: 'center'
+        }}>
+            <Box sx={{
+                width: '324px',
+                height: '180px'
+            }}>
+                {/* credits for @JcToon and DC at https://rive.app/@dc/ */}
+                <RiveComponent />
+            </Box>
+        </Box >
     );
 };
