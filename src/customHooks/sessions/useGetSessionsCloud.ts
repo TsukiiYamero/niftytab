5da12824bcb6b02c0e155cf882494582ee9db820
SaiKisho <diegosferas@gmail.com>
2023-03-23 22:10:06 -0500

    sessions loading issue

--- src/customHooks/sessions/useGetSessionsCloud.ts
+++ src/customHooks/sessions/useGetSessionsCloud.ts
@@ -1,6 +1,6 @@
 import { useAuthState } from '@/contexts/auth';
 import { TabsActions } from '@/contexts/tabs';
-import { useGetTabsContext, useTabsDispatch } from '@/contexts/tabs/hooks';
+import { useTabsDispatch } from '@/contexts/tabs/hooks';
 import { useFetchWithCallback } from '@/customHooks/useFetchWithCallback';
 import { SessionCloud, TabsCloudSupabase } from '@/models';
 import { readAllSessions } from '@/services/tabs';
@@ -9,14 +9,14 @@ import { useCallback, useEffect, useState } from 'react';
 
 export const useGetSessionsCloud = () => {
     const { user } = useAuthState();
-    const { loading } = useGetTabsContext();
     const [dataToShow, setDataToShow] = useState<SessionCloud[]>([]);
+    const [loading, setLoading] = useState(true);
     const { callApi } = useFetchWithCallback();
     const dispatch = useTabsDispatch();
 
     const getSessions = useCallback(async () => {
         if (!user) return;
-
+        setLoading(true);
         dispatch({ type: TabsActions.requestTabs });
 
         const { data, error } = await callApi(readAllSessions, user.id);
@@ -37,6 +37,7 @@ export const useGetSessionsCloud = () => {
         dispatch({ type: TabsActions.updateSessions, payload: SessionCloud });
         dispatch({ type: TabsActions.finishRequestTabs });
         setDataToShow(SessionCloud);
+        setLoading(false);
     }, [callApi, dispatch, user]);
 
     useEffect(() => {
