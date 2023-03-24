a7f6d62f7dba0e678335c5158c227052c1169788
SaiKisho <diegosferas@gmail.com>
2023-03-23 22:08:47 -0500

    Loading issue

--- src/customHooks/tabs/useGetTabsLocal.ts
+++ src/customHooks/tabs/useGetTabsLocal.ts
@@ -1,20 +1,26 @@
 import { TabsActions } from '@/contexts/tabs';
-import { useGetTabsContext, useTabsDispatch } from '@/contexts/tabs/hooks';
+import { useTabsDispatch } from '@/contexts/tabs/hooks';
+import { NiftyTab } from '@/models';
 import { chromeTabsToNiftyTabs, getAllChromeTabs } from '@/utils';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useGetTabsLocal = () => {
     const dispatch = useTabsDispatch();
-    const { local, loading } = useGetTabsContext();
+    const [local, setLocal] = useState<NiftyTab[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getTabs = async () => {
             dispatch({ type: TabsActions.requestTabs });
+            setLoading(true);
 
             const resp = await getAllChromeTabs();
             const dataTabs = chromeTabsToNiftyTabs(resp ?? []);
+
+            setLocal(dataTabs);
             dispatch({ type: TabsActions.updateLocal, payload: dataTabs });
             dispatch({ type: TabsActions.finishRequestTabs });
+            setLoading(false);
         };
         getTabs();
     }, [dispatch]);
