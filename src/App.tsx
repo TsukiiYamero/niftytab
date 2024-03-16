41008132b80011adf76a2af5794274c76ea9d098
SaiKisho <diegosferas@gmail.com>
2024-03-16 13:58:58 -0500

    ⭐ Test chrome function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { AuthProvider } from '@/contexts/auth';
 import { AuthModalProvider } from '@/contexts/authModal';
 import { UserNoAuthenticatedMessage } from '@/ui/molecules/UserNotAuthenticatedMessage';
+import { useGetTabsLocal } from './customHooks/tabs';
+import { Button } from '@nextui-org/react';
 /* import { TabsProvider } from './contexts/tabs';
 import { SnackbarProvider } from './contexts/snackbar/provider';
 import { CustomThemeProvider } from './theme/provider/CustomThemeProvider'; */
 /* import { MainRouter } from './routes/MainRouter'; */
 
 export const App = () => {
+    const { local } = useGetTabsLocal();
+    console.log(local);
+
+    const putTabsToSleep = async () => {
+        /* chrome.tabs.discard({}, function (tabs) {
+            tabs.forEach((tab) => {
+                chrome.tabs.update(tab.id, { active: false });
+            });
+        }); */
+    };
+
     return (
         <AuthProvider>
             {/* <CustomThemeProvider>
@@ -20,6 +33,9 @@ export const App = () => {
             </CustomThemeProvider> */}
             <AuthModalProvider>
                 < UserNoAuthenticatedMessage />
+
+                <Button variant="flat" color="primary" onClick={putTabsToSleep}>
+                </Button>
             </AuthModalProvider>
         </AuthProvider>
     );
