d07ce7950842d50c2959ab794ff0e862d73444ca
SaiKisho <diegosferas@gmail.com>
2024-04-04 18:54:38 -0500

    💄font weight for h1

--- src/ui/organisms/Header/Header.tsx
+++ src/ui/organisms/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header: FC<MainLayoutProps> = ({ textDescription, title }) => {
     return (
         <nav className="flex justify-between items-center h-[70px]">
             <section className='flex flex-col'>
-                <h1 className='text-[length:var(--font-size-secondary-title)]'>{title}</h1>
+                <h1 className='text-[length:var(--font-size-secondary-title)] font-bold'>{title}</h1>
                 <p className='text-[length:14px] text-[--neutral-color-alt-primary]'>
                     {textDescription}
                 </p>
