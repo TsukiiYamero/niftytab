e43519cd39632ecb6505ee53986ed9a768d975fd
SaiKisho <diegosferas@gmail.com>
2024-04-04 14:57:39 -0500

    ♻️ CreateRouter changed for memoryRouter

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,9 @@
 import Home from '@/pages/Home/Home';
 import { Suspend } from '@/pages/Suspend';
 import { HomeRoute, SaveTabsRoute, SuspendRoute } from '@/utils';
-import { createBrowserRouter } from 'react-router-dom';
+import { createMemoryRouter } from 'react-router-dom';
 
-const Routes = createBrowserRouter([{
+const Routes = createMemoryRouter([{
     path: HomeRoute,
     element: <Home />,
     index: true
