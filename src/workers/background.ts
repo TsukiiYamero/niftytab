82aaaed3ccfb63ceb9dda399820e90aef94d65d7
SaiKisho <diegosferas@gmail.com>
2024-09-05 16:31:16 -0500

    ♻️ Refactor logic for bg
    + mock for manual test if onStartup & onChange storage works and it does

--- src/workers/background.ts
+++ src/workers/background.ts
@@ -1,6 +1,33 @@
 import { getUserDataFromSync } from '@/utils/chrome/getUserData';
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Starting Suspension Tabs... FUNCIONA MRD');
-    getUserDataFromSync();
+// On install start the tab suspension
+// this case is not necesary
+chrome.runtime.onInstalled.addListener(async () => {
+    console.log('OnInstalled bg worker');
 });
+
+// On Change user settings in extension
+chrome.storage.onChanged.addListener(async () => {
+    console.log('UserConfig updated restarting bg worker');
+
+    startSuspension();
+});
+
+// On startup browser
+chrome.runtime.onStartup.addListener(() => {
+    // Cargar configuraciones y comenzar el ciclo de suspensión al abrir el navegador
+    console.log('OnStartup bg worker');
+
+    startSuspension();
+});
+
+const startSuspension = async () => {
+    const data = await getUserDataFromSync();
+
+    if (!data) {
+        console.log('No user data found in sync storage');
+        return;
+    }
+
+    console.log(data);
+};
